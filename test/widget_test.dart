32bd7ca0cd90b5ff09ec3abb6c477fabebc8a0d8
dmi6756@yandex.ru <a0zvx753>
2026-08-28 13:21:38 +0300

    create base structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:salon_charodey_front/main.dart';
+import 'package:salon_charodey_front/app/main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
