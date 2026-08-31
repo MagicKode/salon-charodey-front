5fa16d0c3ae757a3e02743210ddc4b41465a1fed
dmi6756@yandex.ru <a0zvx753>
2026-08-31 14:52:38 +0300

    added main_screen logo

--- lib/feature/core/mainlayout/main_layout_body.dart
+++ lib/feature/core/mainlayout/main_layout_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'sections/main_app_bar.dart';
 import 'sections/main_bottom_nav_bar.dart';
+import '../../../uikit/colors/app_colors.dart';
 
 class MainLayoutBody extends StatelessWidget {
   final Widget body;
@@ -18,9 +19,36 @@ class MainLayoutBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Color logoColor = isDark ? AppColors.accentGold : AppColors.primary;
+
     return Scaffold(
       appBar: MainAppBar(isDark: isDark),
-      body: body,
+      body: Stack(
+        children: [
+          // Фоновый логотип (watermark)
+          Positioned.fill(
+            child: Opacity(
+              opacity: 0.06,
+              child: Center(
+                child: ColorFiltered(
+                  colorFilter: ColorFilter.mode(
+                    logoColor,
+                    BlendMode.srcATop,
+                  ),
+                  child: Image.asset(
+                    'assets/logo/light_logo.webp',
+                    width: 300,
+                    height: 300,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Основное содержимое
+          body,
+        ],
+      ),
       bottomNavigationBar: MainBottomNavBar(
         currentIndex: currentIndex,
         onTap: onTap,
